cb2cf9ed1e2be67ae00fae91caeba2f86d3c3337
Peter Bishop <peter_bishop@live.co.uk>
2024-05-10 22:34:36 +0000

    Add js to get the user values and log them

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let userDob = document.getElementById("dob").value;
+let userGender = document.getElementById("gender").value;
+
+let userGaitSpeed = document.getElementById("gait-input").value;
+let userTug = document.getElementById("tug-input").value;
