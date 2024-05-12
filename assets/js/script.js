b165590d181f815a125ad5708504b71242dddbde
Peter Bishop <peter_bishop@live.co.uk>
2024-05-12 22:09:18 +0000

    Modify creation of variables and Add event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,45 @@
-let userDob = document.getElementById("dob").value;
-let userGender = document.getElementById("gender").value;
+//Getting the values
+//Global values (initialised)
+let userDob = "";
+let userGender = "";
+let userGaitSpeed = "";
+let userTug = "";
+const prismaQuestions = ["prisma-1", "prisma-2", "prisma-3", "prisma-4", "prisma-5"];
+const prismaAnswers = {
+    "prisma-1": "no",
+    "prisma-2": "no",
+    "prisma-3": "no",
+    "prisma-4": "no",
+    "prisma-5": "no",
+}
 
-let userGaitSpeed = document.getElementById("gait-input").value;
-let userTug = document.getElementById("tug-input").value;
+// Function to get all values
+function getValues() {
+    userDob = document.getElementById("dob").value;
+    userGender = document.getElementById("gender").value;
+
+
+    prismaQuestions.forEach(question => {
+        const radios = document.getElementsByName(question);
+        radios.forEach(radio => {
+            if (radio.checked) {
+                prismaAnswers[question] = radio.value;
+            }
+        });
+    });
+
+    //Gait Speed
+    userGaitSpeed = document.getElementById("gait-input").value;
+    //TuG
+    userTug = document.getElementById("tug-input").value;
+
+    //Console Logging
+    console.log(userDob);
+    console.log(userGender);
+    console.log(prismaAnswers);
+    console.log(userGaitSpeed);
+    console.log(userTug);
+}
+
+//Event listener for submit button
+document.getElementById("submit-btn").addEventListener("click", getValues);
