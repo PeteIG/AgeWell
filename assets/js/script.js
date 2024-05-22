580dda8f487367b9d2364388c0a32d847de6c1ee
Peter Bishop <peter_bishop@live.co.uk>
2024-05-22 10:40:40 +0000

    Modify UX to hide results section until required assessments completedd

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,30 @@ const prismaAnswers = {
     "prisma-5": "no",
 }
 
-// Function to get all values
+// Get the scroll to top button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+/* Function to switch the results section from hidden to block when assessments have been completed */
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+/* Function to scroll to the top of the document when the user clicks on the button */
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+/* Function to get all values */
 function getValues() {
     userAge = document.getElementById("age-range").value;
     userGender = document.getElementById("gender").value;
@@ -62,7 +85,7 @@ function getValues() {
     console.log(userTug);
 }
 
-//Calculate TuG risk category based on 10 second BGS cutoff 
+/* Calculate TuG risk category based on 10 second BGS cutoff */
 function calculateTugScore(userTug) {
     if (userTug > 10) {
         return "Potential Risk of Frailty";
@@ -71,7 +94,7 @@ function calculateTugScore(userTug) {
     }
 }
 
-//Calculate Gait Speed risk category based on 5 second BGS cutoff 
+/* Calculate Gait Speed risk category based on 5 second BGS cutoff */
 function calculateGaitResult(userGaitSpeed) {
     if (userGaitSpeed > 5) {
         return "Potential Risk of Frailty";
@@ -80,10 +103,26 @@ function calculateGaitResult(userGaitSpeed) {
     }
 }
 
+
+/* Function to count the number of valid assessments, then display the results section if at least 2 valid assessments have been completed, or display a modal warning if this criteria has not been met */
+function showResults() {
+    let validAssessments = 0;
+    if (userGaitSpeed !== "") validAssessments++;
+    if (userTug !== "") validAssessments++;
+
+    if (validAssessments >= 2) {
+        document.getElementById("results").style.display = "block";
+        document.getElementById("submit-btn").style.display = "none";
+    } else {
+        alert("Please complete at least two of the assessments.");
+    }
+}
+
 // Event Listener for the "Submit All" button
 document.getElementById("submit-btn").addEventListener("click", () => {
     getValues();
 
+    /*Function to calculate overall risk of frailty from the 2 or 3 assessments completed */
     function calculateOverallRisk(tugResult, gaitResult) {
         if (tugResult === "Potential Risk of Frailty" || gaitResult === "Potential Risk of Frailty") {
             return "Potential Risk of Frailty";
@@ -97,14 +136,24 @@ document.getElementById("submit-btn").addEventListener("click", () => {
     const gaitResult = calculateGaitResult(userGaitSpeed);
     const overallResult = calculateOverallRisk(tugResult, gaitResult);
 
+    // Display results section
+    showResults();
+
     //Display risk scores
     document.getElementById("tug-result").textContent = `TuG Risk Result: ${tugResult}`;
     document.getElementById("gait-result").textContent = `Gait Speed Risk Result: ${gaitResult}`;
     document.getElementById("overall-result").textContent = `Overall Risk Result: ${gaitResult}`;
+
 });
 
+/* Function to refresh the page */
+function refreshPage() {
+    topFunction();
+    location.reload();
+}
 
-
+// Event Listener for the Reset button
+document.getElementById("reset-btn").addEventListener("click", refreshPage);
 
 // Export variables and functions for testing
 module.exports = {
